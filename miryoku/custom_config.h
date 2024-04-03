e150132f90ee54b3dfbf0d3a4e2c18afe99642f1
ScriBanana <72523821+ScriBanana@users.noreply.github.com>
2024-04-03 12:08:54 +0000

    Correction

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define MIRYOKU_ALTERNATIVES_MEDIA_INVERTEDT_FLIP \
+#define MIRYOKU_LAYER_MEDIA \
 U_RGB_HUI,         U_RGB_SAI,         &kp C_VOL_DN,      U_RGB_BRI,         U_RGB_TOG,         U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 U_RGB_EFF,         &kp C_PREV,        &kp C_VOL_DN,      &kp C_NEXT,        U_EP_TOG,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       &u_out_tog,        U_NA,              &u_to_U_MEDIA,     &u_to_U_FUN,       &kp RALT,          U_NA,              \
